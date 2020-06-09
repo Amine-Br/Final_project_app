fe0cd94dd88bdeabd46574da7d0955bd97aca2a1
FaycalFeddak <62249325+FaycalFeddak@users.noreply.github.com>
2020-06-09 16:16:37 +0100

    dialog

--- app/src/main/java/com/example/projet_final/notif_click.java
+++ app/src/main/java/com/example/projet_final/notif_click.java
@@ -1,14 +1,27 @@
 package com.example.projet_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class notif_click extends AppCompatActivity {
-    private String extra;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.CountDownLatch;
+
+public class notif_click extends AppCompatActivity {
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +38,49 @@ public class notif_click extends AppCompatActivity {
 
     public void getInfoFromNot(){
         String s=getIntent().getStringExtra("key");
-        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+        Dialog dialog=new Dialog(this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        final CountDownLatch done=new CountDownLatch(1);
+
+        FirebaseDatabase.getInstance().getReference().child("users").child(s).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user=dataSnapshot.getValue(User.class);
+                done.countDown();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                done.countDown();
+            }
+        });
+        try {
+            done.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        dialog.setContentView(R.layout.activity_popup);
+        TextView t=dialog.findViewById(R.id.username_popup);
+        t.setText(user.getUser_name());
+        t=dialog.findViewById(R.id.sex_tv2);
+        t.setText(user.getSex());
+        t=dialog.findViewById(R.id.birthday_tv2);
+        t.setText(user.getBirthday());
+        t=dialog.findViewById(R.id.email_tv2);
+        t.setText(user.getEmail());
+        t=dialog.findViewById(R.id.jobs_tv2);
+        t.setText(user.getJobsString());
+        Button b=dialog.findViewById(R.id.prechedule_bitton);
+        b.setVisibility(View.GONE);
+        dialog.setCanceledOnTouchOutside(false);
+        b=dialog.findViewById(R.id.cancel_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        dialog.show();
     }
 
     @Override
